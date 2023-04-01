c3dfbaf76d27fb97892bdb0b18d6c2658dbd1971
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-04-01 00:34:44 -0600

    modified quicksort file

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -4,30 +4,32 @@ public class QuickSort {
     private long comparisons;
     private long swaps;
 
-    public QuickSort(DoublyLinkedList<Movie> movies, int column, boolean creciente){
+    public QuickSort(DoublyLinkedList<Movie> movies, int column, boolean increasing){
         this.sortedList = movies;
-        this.time = sort(column, creciente);
+        this.time = sort(column, increasing);
     }
 
     private long sort(int column, boolean increasing){
 
         long startTime = System.nanoTime();
-
-        quickSort(sortedList,0,(sortedList.size())-1, column, increasing);
-
+        if(increasing){
+        quickSort(sortedList,0,(sortedList.size())-1, column);
+        }else{
+        quickSortDec(sortedList,0,(sortedList.size())-1, column);    
+        }
         long endTime = System.nanoTime();
         return endTime - startTime;
     }
 
-    private void quickSort(DoublyLinkedList<Movie> arr, int left, int right,int col, boolean increasing) {
+    private void quickSort(DoublyLinkedList<Movie> arr, int left, int right,int col) {
         if (left < right) {
-            int pivotIndex = partition(arr, left, right,col, increasing);
-            quickSort(arr, left, pivotIndex - 1,col, increasing);
-            quickSort(arr, pivotIndex + 1, right,col, increasing);
+            int pivotIndex = partition(arr, left, right,col);
+            quickSort(arr, left, pivotIndex - 1,col);
+            quickSort(arr, pivotIndex + 1, right,col);
         }
     }
     
-    private int partition(DoublyLinkedList<Movie> arr, int left, int right, int col, boolean increasing) {
+    private int partition(DoublyLinkedList<Movie> arr, int left, int right, int col) {
         int pivot = arr.index(right).getData().getByID(col);
         int i = left - 1;
     
@@ -43,6 +45,32 @@ public class QuickSort {
     
         return i + 1;
     }
+
+    private void quickSortDec(DoublyLinkedList<Movie> arr, int left, int right,int col) {
+        if (left < right) {
+            int pivotIndex = partitionDec(arr, left, right,col);
+            quickSortDec(arr, left, pivotIndex - 1,col);
+            quickSortDec(arr, pivotIndex + 1, right,col);
+        }
+    }
+    
+    private int partitionDec(DoublyLinkedList<Movie> arr, int left, int right, int col) {
+        int pivot = arr.index(right).getData().getByID(col);
+        int i = left - 1;
+    
+        for (int j = left; j < right; j++) {
+            if (arr.index(j).getData().getByID(col) >= pivot) {
+                i++;
+                swap(arr, i, j);
+                this.comparisons++;
+            }
+        }
+    
+        swap(arr, i + 1, right);
+    
+        return i + 1;
+    }
+
     
     private void swap(DoublyLinkedList<Movie> arr, int i, int j) {
         Movie temp = arr.index(i).getData();
